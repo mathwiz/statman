b5e467a3b0e3c077f4400ef02ced71210482404b
mathwiz <mathwiz@gmail.com>
2018-10-06 14:31:23 -0400

    web scraping

---
+++ nfl/processor.py
@@ -0,0 +1,4 @@
+with open('data/fantasy/2005_rankings.html') as f:
+    for line in f:
+        print(line)
+
---
+++ nfl/scraper.py
@@ -0,0 +1,6 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+html = urlopen("https://www.pro-football-reference.com/years/2017")
+bsObj = BeautifulSoup(html.read(), "lxml")
+print(bsObj.head)
