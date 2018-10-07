1035fe0b5f374361152e593a3f6c4c1863fe3b97
mathwiz <mathwiz@gmail.com>
2018-10-07 12:22:49 -0400

    code dir

--- nfl/code/scraper.py
+++ nfl/code/scraper.py
@@ -3,4 +3,4 @@ from bs4 import BeautifulSoup
 
 html = urlopen("https://www.pro-football-reference.com/years/2017")
 bsObj = BeautifulSoup(html.read(), "lxml")
-print(bsObj.head)
+print(bsObj.table)
