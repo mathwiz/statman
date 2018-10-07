79737e8b76fc620ff54a7e7bc8a37c372fd8eaa9
mathwiz <mathwiz@gmail.com>
2018-10-07 18:20:02 -0400

    new app

--- nfl/app/nfldb/Main.groovy
+++ nfl/app/nfldb/Main.groovy
@@ -1,6 +1,27 @@
+package nfldb
+
 class Main {
 
-  static main(String...args) {
-    println("test")
-  }
+    static main(String... args) {
+        new Main() .start(args)
+    }
+
+    def start(String... args) {
+        show_prompt()
+        def exp
+        while (exp = System.console().readLine()) {
+            if (exp =~ '\\s*[Yy]\\s*') {
+                println "Shooting..."
+            } else {
+                println "Bye."
+                break
+            }
+            show_prompt()
+        }
+    }
+
+    def show_prompt() {
+        //println "Bullet in $gun.loaded"
+        print "Shoot? [y/N] "
+    }
 }
