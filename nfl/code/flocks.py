cc5f3a8cb62150740bfd35a68a0e6108cce6e39c
mathwiz <mathwiz@gmail.com>
2018-10-12 12:57:37 -0400

    flocks alsmost works

--- nfl/code/flocks.py
+++ nfl/code/flocks.py
@@ -5,7 +5,7 @@ import csv
 
 
 prog = sys.argv[0]
-file = '../data/games/Week_4_NFL_Odds.html' if len(sys.argv) < 2 else sys.argv[1]
+file = '../data/games/Week_3_NFL_Odds.html' if len(sys.argv) < 2 else sys.argv[1]
 outfile = 'a.csv' if len(sys.argv) < 3 else sys.argv[2]
 output = open(outfile, mode='w')
 writer = csv.writer(output, lineterminator='\n', quoting=csv.QUOTE_NONNUMERIC)
@@ -19,7 +19,7 @@ def check_season_and_week(line, current_season, week):
 
 
 def find_date(line, season):
-    match = re.match(r'.*<TD>(\d{1,2})\/(\d{1,2}) (\d{1,2})\:(\d\d) ET</TD>.*',line)
+    match = re.match(r'.*<TD>(\d{1,2})\/(\d{1,2}) (\d{1,2})\:(\d\d) ((AM)|(PM)|(ET))</TD>.*',line)
     if match != None:
         d = datetime.datetime(int(season), int(match.group(1)), int(match.group(2)), int(match.group(3)), int(match.group(4)))
         #print(d.date(), d.time())
@@ -66,7 +66,7 @@ def find_money(line):
 
 
 def make_row(season, week, game, line):
-    if not 'date' in game:
+    if 'home_team' not in game:
         print("No home_team", line)
     else:
         key = f'{game["date"].date().year}-{week}-{game["away_team"]}-{game["home_team"]}'
@@ -93,7 +93,7 @@ def make_header():
 
 
 def set_home_away(game, team, home, away):
-    #print(team,home,away)
+    #print(game)
     if home:
         game['home_team'] = team
     elif away:
@@ -111,29 +111,37 @@ def process(file):
     line_num = 999999
     game_total = 0
     make_header()
+    current_line = ""
     with open(file) as f:
         for line in f:
-            line_num += 1
             season, week = check_season_and_week(line, season, week)
-            date = find_date(line, season)
+            current_line += line.lstrip().rstrip()
+            line_num += 1
+            date = find_date(current_line, season)
             if date != None:
                 line_num = 1
                 game = {}
                 game['date'] = date
-            if line_num == 2:
-                game['favorite'], home, neutral = find_team(line)
-                set_home_away(game, game['favorite'], home, neutral)
-            if line_num == 4:
-                game['underdog'], home, neutral = find_team(line)
-                set_home_away(game, game['underdog'], home, neutral)
-            if line_num == 3:
-                game['spread'] = find_number(line)
-            if line_num == 5:
-                game['total'] = find_number(line)
-            if line_num == 6:
                 game_total += 1
-                game['fav_money'], game['und_money'] = find_money(line)
-                make_row(season, week, game, line)
+                current_line = ""
+            if line_num == 2:
+                game['favorite'], home, neutral = find_team(current_line)
+                set_home_away(game, game['favorite'], home, neutral)
+                current_line = ""
+            if line_num == 4:
+                game['underdog'], home, neutral = find_team(current_line)
+                set_home_away(game, game['underdog'], home, neutral)
+                current_line = ""
+            if line_num == 3:
+                game['spread'] = find_number(current_line)
+                current_line = ""
+            if line_num == 5:
+                game['total'] = find_number(current_line)
+                current_line = ""
+            if line_num == 6:
+                game['fav_money'], game['und_money'] = find_money(current_line)
+                make_row(season, week, game, current_line)
+                current_line = ""
     return game_total
 
 
