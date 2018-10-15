1f1e4bae6abd1f59dafee883e78cc5673ceee3d6
mathwiz <yohanlee@yahoo.com>
2018-10-15 12:08:43 -0400

    functions

--- nfl/code/functions.py
+++ nfl/code/functions.py
@@ -8,9 +8,25 @@ def is_int(aString):
         return False
 
 
+def is_float(aString):
+    try:
+        float(aString)
+        return True
+    except ValueError:
+        return False
+
+
 class Test(unittest.TestCase):
     def test_good_int(self):
         self.assertTrue(is_int("17"))
+
+    def test_good_float(self):
+        self.assertTrue(is_float("17"))
+        self.assertTrue(is_float("17.5"))
+
+    def test_bad_number(self):
+        self.assertFalse(is_int("foo"))
+        self.assertFalse(is_float("foo"))
 
 
 if __name__ == '__main__':
