7e6fdef8bccb6bf9bc8ce2808df387b232622b6d
mathwiz <yohanlee@yahoo.com>
2018-10-18 20:36:50 -0400

    record progress

--- nfl/code/ifbdb.py
+++ nfl/code/ifbdb.py
@@ -1,4 +1,6 @@
 import unittest
+import datetime
+import functools
 
 def is_int(aString):
     try:
@@ -16,7 +18,45 @@ def is_float(aString):
         return False
 
 
+def extract_date(aString):
+    try:
+        return datetime.datetime.strptime(aString, '%m/%d/%Y')
+    except:
+        return None
+
+
+def update_past_weeks(history, win):
+    length = len(history)
+    h = history.copy()
+    h.insert(0, 1 if win else 0)
+    return h[:length]
+
+
+def past_wins(history, weeks_past):
+    return functools.reduce(lambda a, x: a + x, history[:weeks_past + 1], 0)
+
+
 class Test(unittest.TestCase):
+
+    def test_past_weeks(self):
+        h = [1, 0, 1, 0, 0, 1, 1, 0, 1]
+        self.assertEqual(1, past_wins(h, 0))
+        self.assertEqual(1, past_wins(h, 1))
+        self.assertEqual(2, past_wins(h, 2))
+        self.assertEqual(2, past_wins(h, 3))
+        self.assertEqual(2, past_wins(h, 4))
+        self.assertEqual(3, past_wins(h, 5))
+        self.assertEqual(4, past_wins(h, 6))
+        self.assertEqual(4, past_wins(h, 7))
+        self.assertEqual(5, past_wins(h, 8))
+
+    def test_past_weeks_update(self):
+        h = [1, 0, 1, 0, 0, 1, 1, 1, 0]
+        h2 = [0, 1, 0, 1, 0, 0, 1, 1, 1]
+        h3 = [1, 1, 0, 1, 0, 0, 1, 1, 1]
+        self.assertEqual(h2, update_past_weeks(h, False))
+        self.assertEqual(h3, update_past_weeks(h, True))
+
     def test_good_int(self):
         self.assertTrue(is_int("17"))
 
@@ -27,6 +67,12 @@ class Test(unittest.TestCase):
     def test_bad_number(self):
         self.assertFalse(is_int("foo"))
         self.assertFalse(is_float("foo"))
+
+    def test_date(self):
+        d = extract_date("12/10/2018")
+        self.assertEqual(12, d.month)
+        self.assertEqual(10, d.day)
+        self.assertEqual(2018, d.year)
 
 
 if __name__ == '__main__':
--- nfl/code/records.py
+++ nfl/code/records.py
@@ -1,27 +1,87 @@
 import sys
 import csv
 import datetime
+import ifbdb as functions
+
+records = {}
+
+def row_key(row):
+    return f'{row["schedule_season"]}-{row["schedule_week"]}-{row["team_away"]}-{row["team_home"]}'
 
 
-def extract_date(aString):
-    try:
-        return datetime.datetime.strptime(aString, '%m/%d/%Y')
-    except:
-        return None
+def key_fields(row):
+    return (row['schedule_season'], row['schedule_week'], row['team_home'], row['team_away'])
+
+
+def make_key(team, season, week):
+    return f'{team}-{season}-{week}'
+
+
+def home_winner(row):
+    home = int(row['score_home'])
+    away = int(row['score_away'])
+    result = home > away
+    #print(home, away, result)
+    return result
+
+
+def tie(row):
+    return int(row['score_home']) == int(row['score_away'])
+
+
+def game_win(row):
+    if tie(row):
+        print("TIE!")
+        return (None, None)
+    elif home_winner(row):
+        print(row['team_home'], row['score_home'], row['team_away'], row['score_away'])
+        return (True, False)
+    else:
+        print(row['team_away'], row['score_away'], row['team_home'], row['score_home'])
+        return (False, True)
+
+
+def add_game(team, season, week, win, loss):
+    key = f'{team}-{season}'
+    if key not in records:
+        records[key] = {'wins': 0, 'losses': 0, 'ties': 0, \
+        'win_history': {0: 0, 1: 0, 2: 0, 3: 0}} #todo: make this span seasons
+    records[key]['wins'] = records[key]['wins'] + (1 if win else 0)
+    records[key]['losses'] = records[key]['losses'] + (1 if loss else 0)
+    records[key]['ties'] = records[key]['ties'] + (0 if win or loss else 1)
+    increment_history(team, season, week, win)
+
+
+def increment_history(team, season, week, win):
+    key = f'{team}-{season}'
+    functions.update_past_weeks(records[key]['win_history'], win)
+
+
+def output_row(season, week, home, away, row):
+    print(season, week, home, away, \
+    records[f'{home}-{season}']['wins'], \
+    records[f'{away}-{season}']['wins'], \
+    records[f'{home}-{season}']['win_history'][1], \
+    records[f'{away}-{season}']['win_history'][1], \
+    records[f'{home}-{season}']['win_history'][2], \
+    records[f'{away}-{season}']['win_history'][2], \
+    records[f'{home}-{season}']['win_history'][3], \
+    records[f'{away}-{season}']['win_history'][3], \
+    "End")
 
 
 def process(file):
     line_num = 0
-    last_date = datetime.datetime(1950, 1, 1)
     with open(file) as f:
         reader = csv.DictReader(f)
         for row in reader:
-            current_line = row['schedule_date']
-            d = extract_date(current_line)
-            if d < last_date:
-                print(row)
-            line_num += 1
-            last_date = d
+            if functions.is_int(row['score_home']):
+                season, week, home, away = key_fields(row)
+                home_win, away_win = game_win(row)
+                add_game(home, season, week, home_win==True, away_win==True)
+                add_game(away, season, week, away_win==True, home_win==True)
+                output_row(season, week, home, away, row)
+                line_num += 1
     print("Processed", line_num, "lines")
 
 
