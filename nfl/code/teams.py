602cc19a542ee68eabbf309b80b64a8e487824d3
mathwiz <yohanlee@yahoo.com>
2018-10-22 22:11:55 -0400

    favorite/underdog

--- nfl/code/teams.py
+++ nfl/code/teams.py
@@ -1,12 +1,26 @@
 import csv
 
 
-def team_favorite(row):
-    return row['team_favorite_id']
+def team_favorite(row, teams):
+    id = row.get('team_favorite_id')
+    if not id or not teams.get(id):
+        return None
+    elif row['team_home'] in teams[id]:
+        return row['team_home']
+    elif row['team_away'] in teams[id]:
+        return row['team_away']
+    return None
 
 
-def team_underdog(row):
-    return row['team_favorite_id']
+def team_underdog(row, teams):
+    id = row.get('team_favorite_id')
+    if not id or not teams.get(id):
+        return None
+    elif row['team_home'] in teams[id]:
+        return row['team_away']
+    elif row['team_away'] in teams[id]:
+        return row['team_home']
+    return None
 
 
 def load_from_reader(reader, teams):
