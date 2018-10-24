fa0e1ddf63267df90122dab7716e99d61f74ce51
mathwiz <yohanlee@yahoo.com>
2018-10-24 15:07:36 -0400

    text labels

--- nfl/code/analysis.R
+++ nfl/code/analysis.R
@@ -3,8 +3,14 @@
 # Startup
 setwd("~/Dev/Github/statman/nfl/code")
 load(".RData")
+library(Rcmdr)
+library(ggplot2)
 library(Hmisc)
 describe(nfl)
 
+# Loading
+ nfl = read.csv("/Users/yohanlee/Dev/Github/statman/nfl/data/games/betting.csv", header=TRUE)
+
 # Analysis
+names(nfl)
 mean(nfl$spread)
