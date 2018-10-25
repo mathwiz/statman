a20236ee199a662475dbcebacefbab7ee9748e69
mathwiz <yohanlee@yahoo.com>
2018-10-25 10:24:36 -0400

    more R

--- nfl/code/analysis.R
+++ nfl/code/analysis.R
@@ -1,16 +1,22 @@
 # Analysis of NFL
 
-# Startup
-setwd("~/Dev/Github/statman/nfl/code")
-load(".RData")
 library(Rcmdr)
+
+# Startup
+projectDir <- "~/Dev/Github/statman/nfl"
+setwd(file.path(projectDir, "code"))
+getwd()
+load(".RData")
 library(ggplot2)
 library(Hmisc)
-describe(nfl)
+dataDir <- file.path(projectDir, "data")
 
 # Loading
- nfl = read.csv("/Users/yohanlee/Dev/Github/statman/nfl/data/games/betting.csv", header=TRUE)
+ nfl2 = read.csv(file.path(dataDir, "games", "betting.csv"), header=TRUE)
+describe(nfl)
 
 # Analysis
 names(nfl)
 mean(nfl$spread)
+levels(nfl$over_under_result)
+subset(nfl, week==17 & season==2017, select=c(home_fav, favorite, underdog, spread, home_recent_scoring, away_recent_scoring, home_recent_allowed, away_recent_allowed, score_home, score_away, spread_diff))
