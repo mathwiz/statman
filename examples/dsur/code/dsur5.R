295bcf04c0168e326643ef3de463a4ddc8b3fa1e
mathwiz <yohanlee@yahoo.com>
2018-10-26 18:51:27 -0400

    QQ plot

--- examples/dsur/code/dsur5.R
+++ examples/dsur/code/dsur5.R
@@ -20,3 +20,10 @@ head(dlf)
 HistDay1 <- ggplot(dlf, aes(day1)) + theme(legend.position="none") + geom_histogram(aes(y=..density..), colour="Black", fill="White") + labs(x="Hygiene Score Day 1", y="Density")
 
 HistDay1 + stat_function(fun=dnorm, args=list(mean=mean(dlf$day1, na.rm=TRUE), sd=sd(dlf$day1, na.rm=TRUE)), colour="Black", size=1)
+
+QQDay1 <- ggplot(dlf, aes(sample=day1)) + stat_qq(colour="Blue")
+QQDay2 <- ggplot(dlf, aes(sample=day2)) + stat_qq(colour="Blue")
+QQDay3 <- ggplot(dlf, aes(sample=day3)) + stat_qq(colour="Blue")
+
+ggsave("QQDay1.png")
+
