90e1aca022b28780df11af72eb3d3d12dc5f40b6
mathwiz <yohanlee@yahoo.com>
2018-10-26 23:28:11 -0400

    more

--- examples/dsur/code/dsur5.R
+++ examples/dsur/code/dsur5.R
@@ -16,6 +16,7 @@ library(psych)
 
 dlf <- read.delim(file.path(dataDir, "DownloadFestival(No Outlier).dat"), header=TRUE)
 head(dlf)
+dlf[1:1, c("ticknumb", "gender")]
 
 HistDay1 <- ggplot(dlf, aes(day1)) + theme(legend.position="none") + geom_histogram(aes(y=..density..), colour="Black", fill="White") + labs(x="Hygiene Score Day 1", y="Density")
 HistDay1 + stat_function(fun=dnorm, args=list(mean=mean(dlf$day1, na.rm=TRUE), sd=sd(dlf$day1, na.rm=TRUE)), colour="Black", size=1)
@@ -33,4 +34,12 @@ ggsave("QQDay1.png")
 describe(cbind(dlf$day1, dlf$day2, dlf$day3))
 describe(dlf[, c("day1", "day2", "day3")])
 
-stat.desc(dlf[, c("day1", "day2", "day3")], basic=FALSE, norm=TRUE)
+round(stat.desc(dlf[, c("day1", "day2", "day3")], basic=FALSE, norm=TRUE), digits=3)
+
+rexam <- read.delim(file.path(dataDir, "RExam.dat"), header=TRUE)
+rexam$uni <- factor(rexam$uni, levels=c(0,1), labels=c("Duncetown University", "Sussex University"))
+head(rexam)
+describe(rexam)
+
+by(data=rexam$exam, INDICES=rexam$uni, FUN=describe)
+
