a2bd93d25a6f50fe5bd8a6341753176a8b889f1c
mathwiz <yohanlee@yahoo.com>
2018-10-26 21:56:11 -0400

    R code

--- examples/dsur/code/dsur5.R
+++ examples/dsur/code/dsur5.R
@@ -18,8 +18,11 @@ dlf <- read.delim(file.path(dataDir, "DownloadFestival(No Outlier).dat"), header
 head(dlf)
 
 HistDay1 <- ggplot(dlf, aes(day1)) + theme(legend.position="none") + geom_histogram(aes(y=..density..), colour="Black", fill="White") + labs(x="Hygiene Score Day 1", y="Density")
-
 HistDay1 + stat_function(fun=dnorm, args=list(mean=mean(dlf$day1, na.rm=TRUE), sd=sd(dlf$day1, na.rm=TRUE)), colour="Black", size=1)
+
+ggplot(dlf, aes(day2)) + theme(legend.position="none") + geom_histogram(aes(y=..density..), colour="Black", fill="White") + labs(x="Hygiene Score Day 2", y="Density") + stat_function(fun=dnorm, args=list(mean=mean(dlf$day1, na.rm=TRUE), sd=sd(dlf$day1, na.rm=TRUE)), colour="Black", size=1)
+
+ggplot(dlf, aes(day3)) + theme(legend.position="none") + geom_histogram(aes(y=..density..), colour="Black", fill="White") + labs(x="Hygiene Score Day 3", y="Density") + stat_function(fun=dnorm, args=list(mean=mean(dlf$day1, na.rm=TRUE), sd=sd(dlf$day1, na.rm=TRUE)), colour="Black", size=1)
 
 QQDay1 <- ggplot(dlf, aes(sample=day1)) + stat_qq(colour="Blue")
 QQDay2 <- ggplot(dlf, aes(sample=day2)) + stat_qq(colour="Blue")
@@ -27,3 +30,7 @@ QQDay3 <- ggplot(dlf, aes(sample=day3)) + stat_qq(colour="Blue")
 
 ggsave("QQDay1.png")
 
+describe(cbind(dlf$day1, dlf$day2, dlf$day3))
+describe(dlf[, c("day1", "day2", "day3")])
+
+stat.desc(dlf[, c("day1", "day2", "day3")], basic=FALSE, norm=TRUE)
