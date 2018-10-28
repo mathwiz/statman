774bceeb90c6e57eefc5e69360c945d5ff6886dc
mathwiz <mathwiz@gmail.com>
2018-10-28 14:54:28 -0400

    chapter 5

--- examples/dsur/code/dsur5.R
+++ examples/dsur/code/dsur5.R
@@ -12,18 +12,30 @@ library(reshape2)
 library(car)
 library(pastecs)
 library(psych)
+library(Rcmdr)
 
 
+# Download Festival data set
 dlf <- read.delim(file.path(dataDir, "DownloadFestival(No Outlier).dat"), header=TRUE)
 head(dlf)
 dlf[1:1, c("ticknumb", "gender")]
+dlf$day1_log <- log(dlf$day1 + 1)
+dlf$day2_log <- log(dlf$day2 + 1)
+dlf$day3_log <- log(dlf$day3 + 1)
+describe(dlf)
 
 HistDay1 <- ggplot(dlf, aes(day1)) + theme(legend.position="none") + geom_histogram(aes(y=..density..), colour="Black", fill="White") + labs(x="Hygiene Score Day 1", y="Density")
 HistDay1 + stat_function(fun=dnorm, args=list(mean=mean(dlf$day1, na.rm=TRUE), sd=sd(dlf$day1, na.rm=TRUE)), colour="Black", size=1)
 
-ggplot(dlf, aes(day2)) + theme(legend.position="none") + geom_histogram(aes(y=..density..), colour="Black", fill="White") + labs(x="Hygiene Score Day 2", y="Density") + stat_function(fun=dnorm, args=list(mean=mean(dlf$day1, na.rm=TRUE), sd=sd(dlf$day1, na.rm=TRUE)), colour="Black", size=1)
+ggplot(dlf, aes(day2)) + theme(legend.position="none") + geom_histogram(aes(y=..density..), colour="Black", fill="White") + labs(x="Hygiene Score Day 2", y="Density") + stat_function(fun=dnorm, args=list(mean=mean(dlf$day2, na.rm=TRUE), sd=sd(dlf$day2, na.rm=TRUE)), colour="Black", size=1)
 
-ggplot(dlf, aes(day3)) + theme(legend.position="none") + geom_histogram(aes(y=..density..), colour="Black", fill="White") + labs(x="Hygiene Score Day 3", y="Density") + stat_function(fun=dnorm, args=list(mean=mean(dlf$day1, na.rm=TRUE), sd=sd(dlf$day1, na.rm=TRUE)), colour="Black", size=1)
+ggplot(dlf, aes(day3)) + theme(legend.position="none") + geom_histogram(aes(y=..density..), colour="Black", fill="White") + labs(x="Hygiene Score Day 3", y="Density") + stat_function(fun=dnorm, args=list(mean=mean(dlf$day3, na.rm=TRUE), sd=sd(dlf$day3, na.rm=TRUE)), colour="Black", size=1)
+
+ggplot(dlf, aes(day1_log)) + theme(legend.position="none") + geom_histogram(aes(y=..density..), colour="Black", fill="White") + labs(x="Log Hygiene Score Day 1", y="Density") + stat_function(fun=dnorm, args=list(mean=mean(dlf$day1_log, na.rm=TRUE), sd=sd(dlf$day1_log, na.rm=TRUE)), colour="Black", size=1)
+
+ggplot(dlf, aes(day2_log)) + theme(legend.position="none") + geom_histogram(aes(y=..density..), colour="Black", fill="White") + labs(x="Log Hygiene Score Day 2", y="Density") + stat_function(fun=dnorm, args=list(mean=mean(dlf$day2_log, na.rm=TRUE), sd=sd(dlf$day2_log, na.rm=TRUE)), colour="Black", size=1)
+
+ggplot(dlf, aes(day3_log)) + theme(legend.position="none") + geom_histogram(aes(y=..density..), colour="Black", fill="White") + labs(x="Log Hygiene Score Day 3", y="Density") + stat_function(fun=dnorm, args=list(mean=mean(dlf$day3_log, na.rm=TRUE), sd=sd(dlf$day3_log, na.rm=TRUE)), colour="Black", size=1)
 
 QQDay1 <- ggplot(dlf, aes(sample=day1)) + stat_qq(colour="Blue")
 QQDay2 <- ggplot(dlf, aes(sample=day2)) + stat_qq(colour="Blue")
@@ -36,9 +48,15 @@ describe(dlf[, c("day1", "day2", "day3")])
 
 round(stat.desc(dlf[, c("day1", "day2", "day3")], basic=FALSE, norm=TRUE), digits=3)
 
+rowMeans(cbind(dlf$day1, dlf$day2, dlf$day3))
+dlf$meanHygiene <- rowMeans(cbind(dlf$day1, dlf$day2, dlf$day3), na.rm=TRUE)
+describe(dlf$meanHygiene)
+l
+# R Exam data set
 rexam <- read.delim(file.path(dataDir, "RExam.dat"), header=TRUE)
 rexam$uni <- factor(rexam$uni, levels=c(0,1), labels=c("Duncetown University", "Sussex University"))
 head(rexam)
+rexam$exam_log <- log(rexam$exam)
 describe(rexam)
 
 by(data=rexam$exam, INDICES=rexam$uni, FUN=describe)
