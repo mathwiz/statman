bacda21d69686ce68e5f649292d5ad04081615e7
mathwiz <yohanlee@yahoo.com>
2019-02-09 12:50:20 -0500

    stats

--- examples/dsur/code/dsur7.R
+++ examples/dsur/code/dsur7.R
@@ -7,3 +7,10 @@ library(Rcmdr)
 library(boot)
 library(car)
 library(QuantPsyc)
+
+
+album1<- read.delim(file.path(dataDir, "Album Sales 1.dat"), header=TRUE)
+albumSalesLM.1<- lm(sales ~ adverts, data=album1, na.action=na.exclude)
+summary(albumSalesLM.1)
+
+
