3d355105ce1d06fa134e0a8c4afc81d4087d968b
mathwiz <yohanlee@yahoo.com>
2019-02-09 11:42:07 -0500

    ch 7

---
+++ examples/dsur/code/dsur7.R
@@ -0,0 +1,9 @@
+projectDir <- "/Users/yohanlee/Dev/GitHub/statman/examples/dsur"
+setwd(file.path(projectDir, "code"))
+dataDir <- file.path(projectDir, "data")
+
+
+library(Rcmdr)
+library(boot)
+library(car)
+library(QuantPsyc)
