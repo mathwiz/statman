1e92a0ce69e1e311173765c72d175148186052de
mathwiz <yohanlee@yahoo.com>
2019-05-02 16:51:52 -0400

    ch15

--- examples/dsur/code/dsur15.R
+++ examples/dsur/code/dsur15.R
@@ -29,3 +29,41 @@ alcoholModel
 ecstasyModel<- wilcox.test(ecstasyDat$wedsBDI, ecstasyDat$sundayBDI, paired=TRUE, correct=FALSE)
 ecstasyModel
 
+
+
+                                        # Multiple independent conditions
+
+soyaDat<- read.delim(file.path(dataDir, "Soya.dat"), header=TRUE)
+summary(soyaDat)
+tail(soyaDat, n=30)
+soyaDat$SoyaLev<- factor(soyaDat$Soya, labels=c("No soya meals per week", "1 soya meal per week", "4 soya meals per week", "7 soya meals per week"))
+levels(soyaDat$SoyaLev)
+
+by(soyaDat$Sperm, soyaDat$SoyaLev, stat.desc, basic=FALSE)
+
+leveneTest(soyaDat$Sperm, soyaDat$SoyaLev, center=median)
+
+soyaModel<- kruskal.test(Sperm ~ SoyaLev, data=soyaDat)
+soyaModel
+
+by(soyaDat$Ranks, soyaDat$SoyaLev, mean)
+
+kruskalmc(Sperm ~ SoyaLev, data=soyaDat)
+
+kruskalmc(Sperm ~ SoyaLev, data=soyaDat, cont="two-tailed")
+
+jonckheere.test(soyaDat$Sperm, soyaDat$Soya)
+
+
+                                        # Multiple related conditions (repeated measures)
+
+dietDat<- read.delim(file.path(dataDir, "Diet.dat"), header=TRUE)
+stat.desc(dietDat)
+
+dietCompleteCases<- na.omit(dietDat) # in this case not necessary as no NA exist
+stat.desc(dietCompleteCases)
+
+friedman.test(as.matrix(dietCompleteCases))
+friedmanmc(as.matrix(dietCompleteCases))
+
+
