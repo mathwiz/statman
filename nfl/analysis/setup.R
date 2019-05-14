458bed1b0e42bb7e1ec5789c7944f375aecf61e8
mathwiz <yohanlee@yahoo.com>
2019-05-14 08:30:34 -0400

    extract common code

---
+++ nfl/analysis/setup.R
@@ -0,0 +1,13 @@
+dataDir<- "../data"
+
+qbDat<- read.csv(file.path(dataDir, "qb.csv"), header=TRUE)
+head(qbDat)
+
+rbDat<- read.csv(file.path(dataDir, "rb.csv"), header=TRUE)
+head(rbDat)
+
+wrDat<- read.csv(file.path(dataDir, "wr.csv"), header=TRUE)
+head(wrDat)
+
+teDat<- read.csv(file.path(dataDir, "te.csv"), header=TRUE)
+head(teDat)
