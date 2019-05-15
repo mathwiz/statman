a3b88b0edc8caca70b07b1fe3f1f1c19c6987d81
mathwiz <yohanlee@yahoo.com>
2019-05-15 10:25:21 -0400

    ch18

--- examples/dsur/code/dsur18.R
+++ examples/dsur/code/dsur18.R
@@ -1,7 +1,55 @@
-library(gmaodels)
+library(gmodels)
 library(MASS)
 
 
                                         # Categorical analysis
 
-raqDat<- read.delim(file.path(dataDir, "raq.dat"), header=TRUE)
+catDat<- read.delim(file.path(dataDir, "cats.dat"), header=TRUE)
+str(catDat)
+CrossTable(catDat$Training, catDat$Dance, fisher=TRUE, chisq=TRUE, expected=TRUE, sresid=TRUE, format="SAS")
+
+catTable<- cbind(c(10, 28), c(114, 48))
+catTable
+CrossTable(catTable, fisher=TRUE, chisq=TRUE, expected=TRUE, sresid=TRUE, format="SAS")
+
+
+                                        # Loglinear analysis
+
+catsDogs<- read.delim(file.path(dataDir, "CatsAndDogs.dat"), header=TRUE)
+str(catsDogs)
+
+table(catsDogs)
+
+justCats<- subset(catsDogs, Animal=="Cat")
+justDogs<- subset(catsDogs, Animal=="Dog")
+
+CrossTable(justCats$Training, justCats$Dance, sresid=TRUE, prop.t=FALSE, prop.c=FALSE, prop.chisq=FALSE, format="SPSS")
+CrossTable(justDogs$Training, justDogs$Dance, sresid=TRUE, prop.t=FALSE, prop.c=FALSE, prop.chisq=FALSE, format="SPSS")
+
+catTable<- xtabs(~ Training + Dance, data=justCats)
+catTable
+
+dogTable<- xtabs(~ Training + Dance, data=justDogs)
+dogTable
+
+catSaturated<- loglm(~ Training + Dance + Training:Dance, data=catTable, fit=TRUE)
+catSaturated
+catNoInteraction<- loglm(~ Training + Dance, data=catTable, fit=TRUE)
+catNoInteraction
+
+par(mfrow=c(2,2))
+mosaicplot(catSaturated$fit, shade=TRUE, main="Cats: Saturated Model")
+mosaicplot(catNoInteraction$fit, shade=TRUE, main="Cats: Expected Values")
+
+
+CatDogTable<- xtabs(~ Animal + Training + Dance, data=catsDogs)
+
+saturated<- loglm(~ Animal*Training*Dance, data=CatDogTable)
+summary(saturated)
+
+threeWay<- loglm(~ Animal + Training + Dance + Animal:Training + Animal:Dance + Training:Dance, data=CatDogTable)
+summary(threeWay)
+
+anova(saturated, threeWay)
+
+mosaicplot(CatDogTable, shade=TRUE, main="Cats and Dogs")
