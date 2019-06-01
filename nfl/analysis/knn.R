fe13feeb00d3f7bfd29c8c1508208acd89b2a892
mathwiz <yohanlee@yahoo.com>
2019-06-01 18:38:07 -0400

    more columns

--- nfl/analysis/knn.R
+++ nfl/analysis/knn.R
@@ -5,7 +5,7 @@ library(ISLR)
 
 
 trimCols<- function(df) {
-    return( df[c("Season", "Rk", "Player", "Age", "G", "DKPt", "PaTDPG", "RuTDPG", "ReTDPG", "PaYPG", "RuYPG", "ReYPG", "PaAPG", "RuAPG", "ReRPG", "NextDKG")] )
+    return( df[c("Season", "Rk", "Player", "Age", "G", "DKPt", "PaAtt", "PaCmp", "ReTgt", "PaTDPG", "RuTDPG", "ReTDPG", "PaYPG", "RuYPG", "ReYPG", "PaAPG", "RuAPG", "ReRPG", "NextDKG")] )
 }
 
 trimRows <- function(df, currentSeason) {
@@ -51,36 +51,34 @@ report <- function(frame, modelCols, season, length=20, k) {
 }
 
 
+## add standardized variables
+qbCols <- c("Age", "PaCmp", "PaAtt", "PaTDPG", "RuTDPG", "PaYPG", "RuYPG", "PaAPG", "RuAPG")
+rbCols <- c("Age", "RuTDPG", "RuYPG", "RuAPG", "ReTDPG", "ReYPG", "ReRPG")
+wrCols <- c("Age", "ReTDPG", "ReYPG", "ReRPG", "RuYPG", "RuAPG", "RuTDPG")
+teCols <- c("Age", "ReTDPG", "ReYPG", "ReRPG")
+head(rbTrim[rbCols])
+head(wrTrim[wrCols])
+
 ## wrangle data for knn
 names(qbDat)
 qbTrim<- trimRows(trimCols(qbDat), 2018)
 rbTrim<- trimRows(trimCols(rbDat), 2018)
 wrTrim<- trimRows(trimCols(wrDat), 2018)
 teTrim<- trimRows(trimCols(teDat), 2018)
-tail(teTrim)
-summary(teTrim$Age)
-nrow(teTrim)
 
-
-## add standardized variables
-qbCols <- c("Age", "PaTDPG", "RuTDPG", "PaYPG", "RuYPG", "PaAPG", "RuAPG")
-rbCols <- c("Age", "RuTDPG", "RuYPG", "RuAPG", "ReTDPG", "ReYPG", "ReRPG")
-wrCols <- c("Age", "ReTDPG", "ReYPG", "ReRPG")
-teCols <- c("Age", "ReTDPG", "ReYPG", "ReRPG")
-head(rbTrim[rbCols])
 
 
 ## Models
-qbReport <- report(frame=qbTrim, modelCols=qbCols, season=2018, length=20, k=10)
+qbReport <- report(frame=qbTrim, modelCols=qbCols, season=2018, length=20, k=6)
 head(qbReport, n=30)
 
-rbReport <- report(frame=rbTrim, modelCols=rbCols, season=2018, length=30, k=10)
+rbReport <- report(frame=rbTrim, modelCols=rbCols, season=2018, length=30, k=6)
 head(rbReport, n=40)
 
-wrReport <- report(frame=wrTrim, modelCols=wrCols, season=2018, length=40, k=10)
+wrReport <- report(frame=wrTrim, modelCols=wrCols, season=2018, length=40, k=6)
 head(wrReport, n=50)
 
-teReport <- report(frame=teTrim, modelCols=teCols, season=2018, length=20, k=10)
+teReport <- report(frame=teTrim, modelCols=teCols, season=2018, length=20, k=6)
 head(teReport, n=30)
 
 allPred<- rbind(rbReport, wrReport, teReport, qbReport)
